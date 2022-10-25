6a01558abee9b5723ccb4a6884779b2bd0019d48
xwsasha <xw.sasha@yandex.ru>
2022-10-25 20:01:51 +0300

    added three files

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 def openFile():
+    try:
+        f1=open('data.txt', 'r')
+        while True:
+            data = f1.readline().split("-")
+            print(data)
+            if data[0] == '':
+                break
+            return data
+        f1.close()
+    except IOError:
+        print(IOError.with_traceback())
 
-
-try:
-    f1=open('data.txt', 'r')
+def dateMonth():
+    data2=openFile()
+    print(data2)
     data1=['31', '12', '1900']
-    while True:
-        data = f1.readline().split("-")
-        print(data)
-        if data[0] == '':
-            break
-        if int(data[0]) < int(data1[0]):
-            data1[0]=data[0]
-        if int(data[1]) < int(data1[1]):
-            data1[1]=data[1]
-
-    f1.close()
+    if int(data2[0]) < int(data1[0]):
+        data1[0] = data2[0]
+    if int(data2[1]) < int(data1[1]):
+        data1[1] = data2[1]
     print(data1[0], "-", data1[1])
-except IOError:
-    print(IOError.with_traceback())
 
 
 
